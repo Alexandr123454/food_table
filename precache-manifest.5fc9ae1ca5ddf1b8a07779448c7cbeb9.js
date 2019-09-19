c5900d66ed4d8abcf0581681e414ef88940f78fd
alexandr <alexandrov6927@gmail.com>
2019-09-19 11:20:01 +0300

    Updates

--- precache-manifest.5fc9ae1ca5ddf1b8a07779448c7cbeb9.js
+++ precache-manifest.5fc9ae1ca5ddf1b8a07779448c7cbeb9.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a097bfe5e15e289abc8f9d9455c85b39",
+    "revision": "f5d6ae93188b010886785d9bfc3a033f",
     "url": "/food_table/index.html"
   },
   {
-    "revision": "e17deea8bc7b3334e7b5",
+    "revision": "44b1d7cf51625304295d",
     "url": "/food_table/static/css/main.caf8aa52.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/food_table/static/js/2.d802a2a2.chunk.js"
   },
   {
-    "revision": "e17deea8bc7b3334e7b5",
-    "url": "/food_table/static/js/main.5110da3e.chunk.js"
+    "revision": "44b1d7cf51625304295d",
+    "url": "/food_table/static/js/main.ff70f11a.chunk.js"
   },
   {
     "revision": "9c368677d78eedeef4bd",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/food_table/precache-manifest.44d29f3cc1a21cbd1278868128538759.js"
+  "/food_table/precache-manifest.5fc9ae1ca5ddf1b8a07779448c7cbeb9.js"
 );
 
 self.addEventListener('message', (event) => {
