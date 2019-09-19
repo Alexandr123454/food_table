5501e16770e554b0b74cb823c126508b2b86957b
alexandr <alexandrov6927@gmail.com>
2019-09-19 11:11:39 +0300

    Updates

---
+++ precache-manifest.44d29f3cc1a21cbd1278868128538759.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a097bfe5e15e289abc8f9d9455c85b39",
+    "url": "/food_table/index.html"
+  },
+  {
+    "revision": "e17deea8bc7b3334e7b5",
+    "url": "/food_table/static/css/main.caf8aa52.chunk.css"
+  },
+  {
+    "revision": "764de67b6bd7c04c2823",
+    "url": "/food_table/static/js/2.d802a2a2.chunk.js"
+  },
+  {
+    "revision": "e17deea8bc7b3334e7b5",
+    "url": "/food_table/static/js/main.5110da3e.chunk.js"
+  },
+  {
+    "revision": "9c368677d78eedeef4bd",
+    "url": "/food_table/static/js/runtime~main.a6f29952.js"
+  }
+]);
